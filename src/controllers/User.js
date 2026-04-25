512265599c82ea2cacc249b9ba6306b814e027a4
Lucas Valpereiro <lucasvalpereiro@hotmail.com>
2026-04-25 11:45:40 -0300

    att

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,20 +1,22 @@
 
 /**
- Desafio para casa:
+Criar uma rota DELETE /users/:id
 
- Criar um rota PUT /users/:id que atualiza os dados de um usuario que 
- já existe.
- 
- Regras:
- 1. Usuario precisa ter id numerico
- 2. No POST o novo usuario deve ser criado com id
- 3. No PUT deve ser atualizado apenas os campos envidos no body
- 
- Exemplo de fakeUsers com id: [{id: 1, name: "Joselito", email: 'joselito@mail.com'}]
+Regras:
+1. A rota deve apagar apenas o suário que tem o mesmo ID recebido
+2. A rota deve retornar um erro 404 quando receber um id que não
+
+Um pouco mais avançado
+No lugar de apagar o usuário da "tabela", adicionar um campo chamado deleted_at, e na lista de usuários retornar 
+apenas o usuário que não tem esse campo definido.
+
+Em projetos grandes é comum não apagar de fatos os registros, e no lugaar de apagar definir apenas 
+uma flag para esse usuário não ser mais considerado nas buscas, assim evita perder dados.
  */
 
 const fakeUsers = [
     {
+        id: 1,
         name: "Wilson",
         email: "wilson69@mail.com"
     }
@@ -24,15 +26,55 @@ async function ListUser(req, res) {
     res.send(fakeUsers);
 }
 
+function FindUser(req, res){
+        const {id} = req.params;
+
+        const user = fakeUsers.find(user => Number(user.id) === Number(id));
+        if(!user) {
+            res.status(404);
+            return res.json({
+                error: 'Not found'
+            });
+        }
+        res.json(user);
+    }
+
 function CreateUser(req, res) {
     const {name, email} = req.body;
-    fakeUsers.push({name, email});
+
+    const id = fakeUsers.length + 1;
+
+    fakeUsers.push({id, name, email});
+    
     res.json({
         message: "Usuario cadastrado com sucesso!"
     });
 }
 
+function UpdateUser(req, res){
+    const {id} = req.params;
+    const {name, email} = req.body;
+
+    fakeUsers.forEach(user => {
+        if(Number(user.id) === Number(id)){
+            user.name = name || user.name;
+            user.email = email || user.email
+        }
+    })
+
+    
+
+   // let usuarioEncontrado = fakeUsers.find(user => Number(user.id) === Number(id));
+    
+    //usuarioEncontrado.name = name || usuarioEncontrado.name
+    //usuarioEncontrado.name = email || usuarioEncontrado.email
+
+    res.send('Usuário atualizado com sucesso!');
+}
+
 module.exports = {
     ListUser,
-    CreateUser
+    FindUser,
+    CreateUser,
+    UpdateUser
 }
